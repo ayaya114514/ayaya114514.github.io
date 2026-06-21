6621017f47071802b51ed7a76df202c01474db19
ayaya114514 <anyangyang2022@gmail.com>
2026-06-21 19:45:35 +0800

    fix(project): clean up checks and mobile navigation

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,12 +1,13 @@
 // @ts-check
 import eslintPluginAstro from 'eslint-plugin-astro'
-import { defineConfig } from "eslint/config";
+import { defineConfig } from 'eslint/config'
 
 export default defineConfig([
-  ...eslintPluginAstro.configs.recommended,
-  // Ignore files
   {
-    ignores: ['public/scripts/*', 'scripts/*', '.astro/', 'src/env.d.ts'],
+    ignores: ['public/scripts/**', 'scripts/**', '.astro/**', 'dist/**', 'src/env.d.ts']
+  },
+  ...eslintPluginAstro.configs.recommended,
+  {
     rules: {
       // override/add rules settings here, such as:
       // "astro/no-set-html-directive": "error"
